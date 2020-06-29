3082313437e889ac475ae2dc1e04c617f69d3034
Utshuk <66883584+Utshuk@users.noreply.github.com>
2020-06-29 17:21:09 +0545

    do-while loop in Java

---
+++ Practice_09/src/practice_09/doWhileLoop.java
@@ -0,0 +1,17 @@
+package practice_09;
+public class doWhileLoop 
+{
+
+    public static void main(String[] args) 
+    {
+      int i = 1;
+      do
+      {
+          System.out.println("Inside do-while loop");
+          i++;
+      }
+      while(i>=5);
+        
+    }
+    
+}
